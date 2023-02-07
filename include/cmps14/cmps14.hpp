9825ea03b32e0de211785af254d10c3a2b65e6f6
EthanG78 <ethangarnier78@gmail.com>
2023-02-06 20:00:10 -0400

    trying to add install rule to cmakelists

---
+++ include/cmps14/cmps14.hpp
@@ -0,0 +1,14 @@
+/**
+ * cmps14.hpp
+ * Author: Ethan Garnier
+*/
+#pragma once
+
+class cmps14
+{
+private:
+    /* data */
+public:
+    cmps14(/* args */);
+    ~cmps14();
+};
---
+++ src/cmps14.cpp
@@ -0,0 +1,13 @@
+/**
+ * cmps14.cpp
+ * Author: Ethan Garnier
+*/
+#include "cmps14/cmps14.hpp"
+
+cmps14::cmps14(/* args */)
+{
+}
+
+cmps14::~cmps14()
+{
+}
